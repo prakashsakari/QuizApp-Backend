3c7b7d66777ca3ec7598cc123b6f35281ed841ae
Prakash Sakari <80476561+prakashsakari@users.noreply.github.com>
2022-09-10 12:22:19 +0530

    enabled cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const quizzes = require("./db/quizzes");
 
 const app = express(); //Creating a server
+app.use(cors());
 
 const PORT = 3000;
 
